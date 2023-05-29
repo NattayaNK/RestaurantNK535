9ee287d0304bf865909e0e99d3d99f8079dfe100
NattayaNK <124546131+NattayaNK@users.noreply.github.com>
2023-05-29 11:52:54 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint ={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "1.ครัวอัปษร",
+        "type": "กรุงเทพฯ",
+        "level": "4.1 ⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.50154100000071,
+          13.755534896528602
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "2. After Yum",
+        "type": "ระยอง",
+        "level": "4.5⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.90010245767252,
+          12.928519279448423
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "3.ร้านอาหารเจ๊ไฝ",
+        "type": "กรุงเทพฯ",
+        "level": "4.5⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.50472268650537,
+          13.752759293147975
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "4.เรือนไทยกุ้งเผา",
+        "type": "อยุธยาฯ",
+        "level": "4.5⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.51324472883556,
+          14.200801819265237
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "5.เจ๊โอว",
+        "type": "กรุงเทพฯ",
+        "level": "4.3⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.52255500004088,
+          13.742591488314872
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "6.เล่าลี่ หูฉลาม",
+    
+        "type": "กรุงเทพฯ",
+        "level": "4.1⭐",
+      },
+      "geometry": {
+        "coordinates": [
+          100.51090045767296,
+          13.740127225365868
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "7.ร้านตู้กับข้าว",
+        "type": "ภูเก็ต",
+        "level": "4.0⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          98.38794130665372,
+          7.8839526622238765
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "8.ร้านลูกไก่ทอง",
+        "type": "กรุงเทพฯ",
+        "level": "4.0⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.6200734576716,
+          13.809710164868008
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "9.กินลม ชมสะพาน",
+        "type": "กรุงเทพฯ",
+        "level": "4.5⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49718745767154,
+          13.766976202056398
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "10.บ้านระเบียงน้ำ",
+        "type": "นนทบุรี",
+        "level": "4.2⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.48297399999672,
+          13.881365440237644
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
